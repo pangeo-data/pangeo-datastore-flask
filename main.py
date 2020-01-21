ee1e5d06a444f46cf3d46b687f948d9d3efdede9
Charles Blackmon-Luca <charlesbluca@gmail.com>
2020-01-21 14:12:36 -0500

    Add options to handle xarray datasets at their own endpoint

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 # [START gae_python37_render_template]
 import intake
 import xarray as xr
@@ -22,7 +8,7 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 Bootstrap(app)
 
-xr.set_options(display_style="text")
+xr.set_options(display_style="html")
 
 catalog_dir = "https://raw.githubusercontent.com/pangeo-data/pangeo-datastore/master/intake-catalogs/"
 master = intake.open_catalog(catalog_dir + "master.yaml")
@@ -47,13 +33,14 @@ def parse(path):
             crumbs.append(f'<li class="active">{item}</li>')
         cat = cat[item]
 
-    if cat.container != "catalog": # only render Intake catalogs
-        return "<p>Only Intake catalogs will be rendered.</p>"
-
-    return render_template("catalog.html", cat=cat,
-                                           url=request.base_url.rstrip("/"),
-                                           crumbs=crumbs)
-
+    if cat.container == "catalog": # only render Intake catalogs
+        return render_template("catalog.html", cat=cat,
+                                               url=request.base_url.rstrip("/"),
+                                               crumbs=crumbs)
+    elif cat.container == "xarray":
+        return render_template("xarray.html", cat=cat,
+                                              url=request.base_url.rstrip("/"),
+                                              crumbs=crumbs)
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
