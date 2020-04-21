33e94c9898d166f1f3fa2e8228fbea8db39fdf56
Charles Blackmon-Luca <charlesbluca@gmail.com>
2020-04-20 21:56:19 -0400

    Create du-pangeo-data.py

---
+++ scripts/du-pangeo-data.py
@@ -0,0 +1,23 @@
+# auto-generate some GCS metrics
+
+from gcsfs import GCSFileSystem
+
+fs = GCSFileSystem('pangeo-181919')
+
+# https://stackoverflow.com/questions/1094841/reusable-library-to-get-human-readable-version-of-file-size
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
+
+with open('du-pangeo-data.csv', 'w') as f:
+    f.write('directory, size, nbytes')
+    print('directory, size, nbytes')
+    for folder in fs.ls('pangeo-data'):
+        nbytes = fs.du(folder)
+        f.write(f'{folder}, {sizeof_fmt(nbytes)}, {nbytes}')
+        print(f'{folder}, {sizeof_fmt(nbytes)}, {nbytes}')
+
+fs.put('du-pangeo-data.csv', 'pangeo-data/du-pangeo-data.csv')
